64794dc76e6ec0369c335e64f170ff93e96c59b8
Kevin Fitzgerald <143578317+KFitz1123@users.noreply.github.com>
2024-11-25 20:43:31 +0000

    Updated_Graph

--- AlphaBetaGrph.java
+++ AlphaBetaGrph.java
@@ -1,12 +1,11 @@
+
 /*
-Javafx + jdk 23.0.1 required
+Javafx and jdk23.0.1 required
 */
+
 import javafx.application.Application;
-import javafx.geometry.Pos;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -15,17 +14,31 @@ import javafx.scene.layout.GridPane;
 public class AlphaBetaGrph extends Application {
  public int GRIDSIZE = 20;
  public int SCREENSIZE = 960;
+ public int xylength = GRIDSIZE;
+ public int latSize = 12;
+ public int numnodes = latSize*latSize;
+    
+    public double[] initArrs(double[] Arr, double from, double to){
+        for(int r = 0; r < xylength; r++){
+            Arr[r] =from - (((from - to)/xylength)*r);
+            System.out.println(Arr[r] + "\n");
+        }
+        return Arr;
+    }
+
     @Override
-    public void start(Stage stage) {
+    public void start(@SuppressWarnings("exports") Stage stage) {
         System.out.println("System Running");
 
         Driver driver = new Driver();
-        double[] Alphas = {1, 0.95,0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05 };
-        double[] Betas = {2, 1.95,1.9, 1.85, 1.8, 1.75, 1.7, 1.65, 1.6, 1.55, 1.5, 1.45, 1.4, 1.35, 1.3, 1.25, 1.2, 1.15, 1.1, 1.05 };
-        int numberOfRuns = 20;
+        double[] Alphas = new double[xylength];
+        double[] Betas = new double[xylength];
+        initArrs(Alphas, 1, 0);
+        initArrs(Betas, 2, 1);
+        int numberOfRuns = 5;
         int[][] returned = new int[numberOfRuns][3];
         double[] Averages = { 0, 0, 0 };
-        String[][] graph = new String[Alphas.length][Betas.length];
+        double[][] graph = new double[Alphas.length][Betas.length];
         System.out.println(Alphas.length)
         ;System.out.println(Betas.length);
         int alph = 0;
@@ -38,7 +51,7 @@ public class AlphaBetaGrph extends Application {
                 }
                 Averages[2] = Averages[2] / numberOfRuns;
                 Averages[2] = Math.round(Averages[2] * 100) / 100.0;
-                graph[alph][beta] = pickcolor(Averages[2] / 144);
+                graph[alph][beta] = Averages[2] / numnodes;
                 Averages[0] = 0;
                 Averages[1] = 0;
                 Averages[2] = 0;
@@ -49,9 +62,9 @@ public class AlphaBetaGrph extends Application {
         }
        
         GridPane grid = new GridPane();
-        for(int c = 0; c < 20; c++ ){
-            for(int r = 0; r < 20; r++){
-                    grid.add(new Rectangle(SCREENSIZE/GRIDSIZE,SCREENSIZE/GRIDSIZE, Color.web(graph[r][c], .8)), c, r);
+        for(int c = 0; c < xylength; c++ ){
+            for(int r = 0; r < xylength; r++){
+                    grid.add(new Rectangle(SCREENSIZE/GRIDSIZE,SCREENSIZE/GRIDSIZE, Color.web(pickcolor(graph[r][c]), 1)), c, r);
                 }
             }
         Scene scene_3 = new Scene(grid, SCREENSIZE, SCREENSIZE);
@@ -71,7 +84,7 @@ public class AlphaBetaGrph extends Application {
         }if(mortality < .5){
             return "87CEFA";
         }if(mortality < .6){
-            return "48D1CC";
+            return "00BFFF";
         }if(mortality < .7){
             return "1E90FF";
         }if(mortality < .8){
